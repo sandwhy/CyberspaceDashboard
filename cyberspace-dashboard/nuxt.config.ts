96b1ea7ac9f9994ae79219c21b0a984f745862ab
sandwhy <sandwhen@gmail.com>
2026-07-16 11:13:05 +0700

    frontend backend env change

--- cyberspace-dashboard/nuxt.config.ts
+++ cyberspace-dashboard/nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: (process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5000').replace(/\/$/, '')
+      apiBase: (process.env.BACKEND_URL || 'http://foke-ou.com').replace(/\/$/, '')
     }
   },
   devServer: {
